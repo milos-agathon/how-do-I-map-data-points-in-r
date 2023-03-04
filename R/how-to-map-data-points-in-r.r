a79cc66a814f93001dd83d54bcb953ef7019c308
Milos Popovic <37574511+milos-agathon@users.noreply.github.com>
2023-03-04 20:40:00 +0100

    Update how-to-map-data-points-in-r.r

--- R/how-to-map-data-points-in-r.r
+++ R/how-to-map-data-points-in-r.r
@@ -3,8 +3,6 @@
 #                 Milos Popovic
 #                 2023/03/04
 ################################################################################
-
-setwd("C:/users/milos/Downloads")
 # libraries we need
 libs <- c(
     "tidyverse", "stringr", "httr", "sf",
@@ -21,14 +19,8 @@ if (any(installed_libs == F)) {
 invisible(lapply(libs, library, character.only = T))
 
 
-
-
 # DATA
 #-----
-
-
-
-
 file_name <- "geonames-population-1000.csv"
 ### get Geonames table on places with population >= 1000
 get_geonames_data <- function() {
@@ -43,8 +35,6 @@ get_geonames_data <- function() {
 
 get_geonames_data()
 
-
-
 ### load the table
 load_geonames_data <- function() {
     places_df <- read.csv(file_name, sep = ";")
@@ -55,28 +45,13 @@ places_df <- load_geonames_data()
 
 head(places_df)
 
-
-
-
-
-
 names(places_df)
-
-
-
-
-
-
-
-
 
 places_modified_df <- places_df[, c(2, 7, 14, 20)]
 names(places_modified_df) <-
     c("name", "country_code", "pop", "coords")
 
 head(places_modified_df)
-
-
 
 
 places_modified_df[c("lat", "long")] <-
@@ -88,15 +63,6 @@ places_clean_df <- places_modified_df |>
     dplyr::select(-coords)
 
 head(places_clean_df)
-
-
-
-
-
-
-
-
-
 
 
 # define longlat projection
@@ -118,9 +84,6 @@ ggplot() +
         data = places_sf,
         color = "#7d1d53", fill = "#7d1d53"
     )
-
-
-
 
 
 ggplot() +
@@ -229,133 +192,3 @@ ggplot() +
             legend.background = element_rect(fill = "white", color = NA),
             panel.border = element_blank(),
         )
-    
-
-
-
-
-
-
-# ggplot2 theme
-theme_for_the_win <- function() {
-    theme_minimal() +
-        theme(
-            axis.line = element_blank(),
-            axis.text.x = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks = element_blank(),
-            axis.title.y = element_blank(),
-            legend.position = c(.5, 1),
-            legend.text = element_text(size = 60, color = "grey20"),
-            legend.title = element_text(size = 70, color = "grey20"),
-            legend.spacing.y = unit(0.25, "cm"),
-            legend.spacing.x = unit(0.25, "cm"),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            plot.margin = unit(
-                c(t = 0, r = 0, b = 0, l = 0), "lines"
-            ),
-            plot.background = element_rect(fill = "#f5f5f2", color = NA),
-            panel.background = element_rect(fill = "#f5f5f2", color = NA),
-            legend.background = element_rect(fill = "#f5f5f2", color = NA),
-            panel.border = element_blank(),
-        )
-}
-
-
-
-
-
-
-# function for labelling regions
-label_regions <- function() {
-    ggrepel::geom_text_repel(deu_coords[1:5, ],
-        mapping = aes(x = long, y = lat, label = NAME_LATN),
-        colour = "grey20",
-        family = "Montserrat",
-        fontface = "bold",
-        size = 20,
-        segment.colour = "grey20",
-        segment.alpha = .9,
-        segment.linetype = 3,
-        segment.size = .25,
-        nudge_x = .95,
-        nudge_y = .15,
-        direction = "x"
-    )
-}
-
-# CONSTANTS
-#------------
-# define projections
-# longlat
-crsLONGLAT <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-# Lambert
-crsLAEA <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +datum=WGS84 +units=m +no_defs"
-
-
-
-# colors
-cols <- rev(c(
-    "#140e26", "#451a40",
-    "#7d1d53", "#b32957",
-    "#eb804e", "#ffdc58",
-    "#eae2b7"
-))
-
-make_point_map <- function() {
-    p2 <-
-        ggplot() +
-        geom_sf(
-            data = deu_polygon,
-            fill = "transparent",
-            color = "grey20",
-            size = .1
-        ) +
-        geom_sf(
-            data = deu_points,
-            mapping = aes(
-                size = pop_1000s,
-                geometry = geometry
-            ), color = cols[5],
-            alpha = .5
-        ) +
-        label_regions() +
-        scale_size(
-            breaks = breaks,
-            range = c(1, 10),
-            labels = round(breaks, 0),
-            limits = c(vmin, vmax),
-            name = ""
-        ) +
-        guides(
-            color = "none",
-            size = guide_legend(
-                direction = "horizontal",
-                title.position = "top",
-                title.hjust = 0.5,
-                label.hjust = 0,
-                nrow = 1,
-                byrow = F,
-                reverse = F,
-                label.position = "bottom"
-            )
-        ) +
-        theme_for_the_win() +
-        labs(
-            y = "",
-            subtitle = "",
-            x = "",
-            title = "",
-            caption = ""
-        )
-    return(p2)
-}
-
-map2 <- make_point_map()
-ggsave(
-    filename = "germany_population_point.png",
-    width = 6, height = 8.5, dpi = 600, device = "png",
-    bg = "white", map2
-)
-
